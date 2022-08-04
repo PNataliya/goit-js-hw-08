8e15e3ed9b0e584cc3c0ad2d63456482891f3cb5
Nataliya Pogrebniak <pogrebnyak.nataliya@gmail.com>
2022-08-04 22:18:50 +0300

    change tasks 2,3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -21,6 +21,8 @@
 
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
@@ -29,24 +31,25 @@ const formData = {};
 
 function onFormData(e) {
   formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 
   e.preventDefault();
   e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 (function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  let data = JSON.parse(localStorage.getItem(STORAGE_KEY));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
+
   if (data) {
-    email.value = data;
-    message.value = data;
+    email.value = data.email;
+    message.value = data.message;
   }
 
   console.log(data);
